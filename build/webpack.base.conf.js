0cad4dae8323548489d9de7c7d43963c6465ae2a
Kirill Chetvertkoff <chetvertkoff.kirill@gmail.com>
2019-07-28 14:46:19 +0300

    Version 2.0

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,12 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const pngquant = require('pngquant');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
 
 const PATHS = {
   src: path.join(__dirname, '../src'),
@@ -23,7 +29,7 @@ module.exports = {
     splitChunks: {
       cacheGroups: {
         vendor: {
-          name: 'vendors',
+          filename: 'assets/js/vendor.js',
           test: /node_modules/,
           chunks: 'all',
           enforce: true
@@ -47,7 +53,7 @@ module.exports = {
       options: {
         name: '[name].[ext]'
       }
-    }, {
+    },{
       test: /\.sass$/,
       use: [
         'style-loader',
@@ -80,19 +86,25 @@ module.exports = {
   },
 
   plugins: [
-    
+    // new CompressionPlugin({
+    //   test: /\.(js|css|html|svg)$/,
+    // }),
+    // new BundleAnalyzerPlugin(),
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].css`,
     }),
-    // Copy HtmlWebpackPlugin and change index.html for another html page
     new HtmlWebpackPlugin({
       template: `${PATHS.src}/index.html`,
       filename: './index.html',
       inject: true
     }),
     new CopyWebpackPlugin([
-      { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+      { from: `${PATHS.src}/img`, to: `${PATHS.assets}/img` },
       { from: `${PATHS.src}/static`, to: '' },
-    ])
+    ]),
+    new ImageminPlugin({
+      pngquant: ({quality: 50}),
+      plugins: [imageminMozjpeg({quality: 50})]
+    })
   ],
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './css/main.css'
 import './scss/main.scss'
 import './sass/main.sass'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-//
+
 
 
 // window.Vue=require('vue')
-// import './js/React/index.js'
+import './js/React/index.js'
 
 import './js/common.js'
